b4c79780a3b0dd65077c24c4c84e1d147f4bece5
sommerfeldlp <leon.s@aol.com>
2025-10-01 17:44:15 +0200

    feat: necessary functions for console display, add reference to snakegame

--- SnakeSolution/SnakeGame.Console/Program.cs
+++ SnakeSolution/SnakeGame.Console/Program.cs
@@ -1,3 +1,59 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using SnakeGame;
+using System;
+using System.Threading;
 
-Console.WriteLine("Hello, World!");
+class Program
+{
+    static void Main()
+    {
+        
+    }
+
+    static void Render(Game game)
+    {
+        // pretty simple renderer setup: clear and draw everything
+        Console.Clear();
+
+        for (int y = 0; y < game.Board.Height; y++)
+        {
+            for (int x = 0; x < game.Board.Width; x++)
+            {
+                Console.SetCursorPosition(x, y);
+                Console.Write('.');
+            }
+        }
+        
+        // food
+        var foodPos = game.Food.Position;
+        Console.SetCursorPosition(foodPos.X, foodPos.Y);
+        Console.Write('*');
+        
+        // snake
+        bool first = true;
+        foreach (var seg in game.Snake.Body)
+        {
+            Console.SetCursorPosition(seg.X, seg.Y);
+            Console.Write(first ? '0' : 'o');
+            first = false;
+        }
+        
+        // portray score
+        Console.SetCursorPosition(0, game.Board.Height);
+        Console.Write($"Score: {game.Score}");
+    }
+
+    static Direction? KeyToDirection(ConsoleKey key)
+    {
+        switch (key)
+        {
+            case ConsoleKey.UpArrow:
+                return Direction.Up;
+            case ConsoleKey.DownArrow:
+                return Direction.Down;
+            case ConsoleKey.LeftArrow:
+                return Direction.Left;
+            case ConsoleKey.RightArrow:
+                return Direction.Right;
+        }
+    }
+}
