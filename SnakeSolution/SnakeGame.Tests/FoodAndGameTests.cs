391ddc72c3b3836ac916cc0903a00e3e8385b240
Leon <leon.s@aol.com>
2025-10-07 11:33:29 +0200

    test: implement 3 unit tests
    snake tests: moving updates the head position, changing directions prevents reversing

--- SnakeSolution/SnakeGame.Tests/FoodAndGameTests.cs
+++ SnakeSolution/SnakeGame.Tests/FoodAndGameTests.cs
@@ -1,11 +1,22 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using SnakeGame;
-using System.Collections.Generic;
 
 namespace SnakeGame.Tests;
 
 [TestClass]
 public class FoodAndGameTests
 {
-    
+    [TestMethod]
+    public void NoFoodRespawnOnSnake()
+    {
+        Board board = new Board(10, 10);
+        Snake snake = new Snake(new Position(5, 5));
+        
+        snake.Move(true);
+        snake.Move(true);
+        
+        Food food = new Food(new System.Random(0));
+        food.Respawn(board, snake.Body);
+        
+        CollectionAssert.DoesNotContain(snake.Body.ToList(), food.Position);
+    }
 }
--- SnakeSolution/SnakeGame.Tests/SnakeTests.cs
+++ SnakeSolution/SnakeGame.Tests/SnakeTests.cs
@@ -1,11 +1,28 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using SnakeGame;
-using System.Linq;
 
 namespace SnakeGame.Tests;
 
 [TestClass]
 public class SnakeTests
 {
+    [TestMethod]
+    public void MovingUpdatesHeadPosition()
+    {
+        Position spawnPosition = new Position(5, 5);
+        Snake snake = new Snake(spawnPosition);
+        snake.Move();
+        Position head = snake.Head;
+        
+        Assert.AreEqual(spawnPosition, head);
+    }
     
+    [TestMethod]
+    public void DirectionChangePreventsReverse()
+    {
+        Snake snake = new Snake(new Position(5, 5));
+        // standard direction is right, choosing the opposite should always be ignored
+        snake.ChangeDirection(Direction.Left);
+        
+        Assert.AreEqual(Direction.Right, snake.Direction);
+    }
 }
